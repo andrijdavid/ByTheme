8bb9053ec4a5a8d1aac2962295022ed8702ecbde
andrijdavid@hotmail.com <andrijdavid@gmail.com>
2016-03-12 12:09:14 +0300

    fix comment in header

--- resources/views/partials/header/header.scout.php
+++ resources/views/partials/header/header.scout.php
@@ -9,25 +9,25 @@
         {{--</div>--}}
         {{--<div class="collapse navbar-collapse">--}}
             {{--<div class="bottom-header">--}}
-                {{--<a class="navbar-brand capitalize" href="<?php esc_url( home_url() ) ?>" title="<?php bloginfo('name') ?>">--}}
+                {{--<a class="navbar-brand capitalize" href="<?php //esc_url( home_url() ) ?>" title="<?php //bloginfo('name') ?>">--}}
                     {{--@if(empty( Option::get("theme-option-image", "logo") ))--}}
-                    {{--<?php bloginfo('name') ?>--}}
+<!--                    --><?php //bloginfo('name') ?>
                     {{--@else--}}
-                    {{--<img src="{{ Option::get(" theme-option-image", "logo") }}" alt="<?php bloginfo('name') ?>">--}}
+                    {{--<img src="{{ Option::get(" theme-option-image", "logo") }}" alt="<?php //bloginfo('name') ?>">--}}
                     {{--@endif--}}
                 {{--</a>--}}
                 {{--<div class="container-fluid">--}}
-                    {{--<?php wp_nav_menu(array(--}}
-                        {{--'theme_location' => 'header',--}}
-                        {{--'depth' => 2,--}}
-                        {{--'container' => 'div',--}}
-                        {{--'menu_id' => 'navbar',--}}
-                        {{--//'container_class' => 'nav-container  navbar-collapse collapse',--}}
-                        {{--//'container_id' => 'navbar',--}}
-                        {{--'menu_class' => 'nav navbar-nav navbar-right main-menu',--}}
-                        {{--'fallback_cb' => 'wp_bootstrap_navwallker::fallback',--}}
-                        {{--'walker' => new wp_bootstrap_navwalker(),--}}
-                    {{--)) ?>--}}
+                    <?php /*wp_nav_menu(array(
+                        'theme_location' => 'header',
+                        'depth' => 2,
+                        'container' => 'div',
+                        'menu_id' => 'navbar',
+                        //'container_class' => 'nav-container  navbar-collapse collapse',
+                        //'container_id' => 'navbar',
+                        'menu_class' => 'nav navbar-nav navbar-right main-menu',
+                        'fallback_cb' => 'wp_bootstrap_navwallker::fallback',
+                        'walker' => new wp_bootstrap_navwalker(),
+                    )) */?>
 
                     {{--<div class="nav navbar-form navbar-rigth search-field">--}}
                         {{--@include('partials.header.search')--}}
@@ -36,13 +36,13 @@
             {{--</div>--}}
             {{--<div class="top-header">--}}
                 {{--<div class="container-fluid">--}}
-                    {{--<?php--}}
-                    {{--wp_nav_menu([--}}
-                        {{--'theme_location' => 'topnav',--}}
-                        {{--'menu_id' => 'topnav',--}}
-                        {{--'menu_class' => 'nav navbar-nav navbar-right main-menu'--}}
-                    {{--])--}}
-                    {{--?>--}}
+                    <?php
+                    /*wp_nav_menu([
+                        'theme_location' => 'topnav',
+                        'menu_id' => 'topnav',
+                        'menu_class' => 'nav navbar-nav navbar-right main-menu'
+                    ])*/
+                    ?>
                 {{--</div>--}}
             {{--</div>--}}
         {{--</div><!--/.nav-collapse -->--}}
