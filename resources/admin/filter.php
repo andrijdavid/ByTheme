bd77cddcdd5d049e1f1e8e613376c2516a1e36e6
andrijdavid@hotmail.com <andrijdavid@gmail.com>
2016-03-12 11:15:43 +0300

    fix slider height and add serach on navbar

--- resources/admin/filter.php
+++ resources/admin/filter.php
@@ -12,3 +12,9 @@
 /**
  * Add Source Name and URL fields to media uploader
  */
+
+function add_last_nav_item($items) {
+    $p = View::make('partials.header.search')->render();
+    return $items . $p;
+}
+add_filter('wp_nav_menu_items','add_last_nav_item');
--- resources/views/partials/header/search.scout.php
+++ resources/views/partials/header/search.scout.php
@@ -1,29 +1,13 @@
 <li>
-    <div class="nav-search right">
-        <a href="#" onclick="return false;" class="search-open">
-            <i class="mdi-action-search"></i>
-        </a>
-        <div
-            class="material-search-overlay circle material-search-overlay-color-bg light-blue darken-2 material-search-overlay-hidden"></div>
-        {{ Form::open(home_url('/'), 'GET', false, [
-        'class'=>'material-search-form material-search-form-hidden'
-        ])}}
-        <div class="material-search-form-container">
-            <a href="#" onclick="return false;" class="material-search-close">
-                <i class="mdi-navigation-close"></i>
-            </a>
-
-            <div class="material-search-form-container-row">
-                <div class="material-search-label center-align">
-                    <label for="search"><h1><?php _e('Search', THEME_TEXT_DOMAIN) ?></h1></label>
-                </div>
-                <div class="material-search-input">
-                    <input id="search" type="text" name="s" class="flow-text" autofocus autocomplete="off">
-                    <span class="input-bottom-bar"></span>
-                </div>
-            </div>
-        </div>
-        {{ Form::close()}}
-
+    {{ Form::open(home_url('/'), 'GET', false, [
+'class'=>'navbar-form navbar-left',
+'role' => 'search'
+])}}
+    <div class="form-group">
+        <input type="text" class="form-control" placeholder="<?php _e('Search', THEME_TEXT_DOMAIN) ?>">
     </div>
+    <button type="submit" class="btn btn-default">
+        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+    </button>
+    {{ Form::close()}}
 </li>
