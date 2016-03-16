f42e27bf8ffe43ea6167a2f824cbb62850c2899f
andrijdavid@hotmail.com <andrijdavid@gmail.com>
2016-03-16 09:34:04 +0300

    fix page view

--- resources/views/pages/singular.scout.php
+++ resources/views/pages/singular.scout.php
@@ -2,7 +2,23 @@
 
 @section('content')
 <div class="container pt-50">
+    @loop
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1 col-lg-offset-1 col-lg-10 col-xs-12 col-sm-12 bg-white">
+            <div class="p-5 m-5 fit">
+                <article>
+                    <header>
+                        <h1 class="text-center">{{\Themosis\Facades\Loop::title()}}</h1>
+                    </header>
+                    <section>
+                        {{\Themosis\Facades\Loop::content() }}
+                    </section>
+                </article>
 
+            </div>
+        </div>
+    </div>
+    @endloop
 </div>
 @stop
 
